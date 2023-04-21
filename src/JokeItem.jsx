fa37c080dec72fba30a7f6046fe1b478ebe974cc
PATRICK MALLERY <patrick@openfoodnetwork.org.uk>
2023-04-21 08:02:27 +0100

    added wireframe pngs to repo

--- src/JokeItem.jsx
+++ src/JokeItem.jsx
@@ -5,8 +5,10 @@ export default function JokeItem({joke, addEmoji, index, remove, flag}) {
     return (
         <li  className= {joke.flagged === true ? "quote-item-container-flagged" : "quote-item-container"}>   
             <p className= {joke.flagged === true ?"quote-item-flagged" : "quote-item"}>{joke.joke}</p>
+
             <button className= {joke.flagged === true ? "remove-flagged" :"remove"} onClick={(e) => remove(e, index)}>X</button>
             <div className={joke.flagged === true ? "emoji-flag-flagged" :"emoji-flag"} onClick={(e) => flag(e, index)}>🏴</div>
+
             <div className="emoji-container">
                 <div className={joke.flagged === true ? "emoji-flagged" :"emoji"} id="👊" onClick={(e) => addEmoji(e, index)}>👊</div>
                 <div className={joke.flagged === true ? "emoji-flagged" :"emoji"} id="🔫" onClick={(e) => addEmoji(e, index)}>🔫</div>
